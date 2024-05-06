6be599df0e96b9b977d34f0bf02eb249ab2002c9
tim <tim@twa.ninja>
2024-05-06 17:32:37 -0600

    Add untracked files; i2c basic read working

--- src/main.cpp
+++ src/main.cpp
@@ -11,26 +11,7 @@
 #include "gp2040aux.h"
 
 #include <cstdlib>
-
-// // Debug Printing via UART:
-// #include <stdio.h>
-// #include "pico/stdlib.h"
-// #include "hardware/uart.h"
-
-// #define UART_ID uart1
-// #define BAUD_RATE 115200
-// #define UART_TX_PIN 24
-// #define UART_RX_PIN 21
-
-// // Set up our UART with the required speed.
-// uart_init(UART_ID, BAUD_RATE);
-// // Set the TX and RX pins by using the function select on the GPIO
-// // Set datasheet for more information on function select
-// gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
-// gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
-
-// // Send out a string, with CR/LF conversions
-// uart_puts(UART_ID, " UART Debug Enabled!\r\n");
+#include "i2c_analog_tla2528.h"
 
 // Custom implementation of __gnu_cxx::__verbose_terminate_handler() to reduce binary size
 namespace __gnu_cxx {
@@ -40,34 +21,28 @@ void __verbose_terminate_handler()
 }
 }
 
-static GP2040 * gp2040Core0 = nullptr;
-static GP2040Aux * gp2040Core1 = nullptr;
-
 // Launch our second core with additional modules loaded in
 void core1() {
 	multicore_lockout_victim_init(); // block core 1
 
-	// Create GP2040 w/ Additional Modules for Core 1	
+	// Create GP2040 w/ Additional Modules for Core 1
+	GP2040Aux * gp2040Core1 = new GP2040Aux();
 	gp2040Core1->setup();
 	gp2040Core1->run();
 }
 
 int main() {
 	// Create GP2040 Main Core (core0), Core1 is dependent on Core0
-	gp2040Core0 = new GP2040();
-	gp2040Core1 = new GP2040Aux();
+	GP2040 * gp2040 = new GP2040();
+	gp2040->setup();
 
-	// Create GP2040 Main Core - Setup Core0
-	gp2040Core0->setup();
+	I2CAnalogTLA2528 * tla2528 = new I2CAnalogTLA2528();
+	tla2528->test();
 
 	// Create GP2040 Thread for Core1
 	multicore_launch_core1(core1);
 
-	// Sync Core0 and Core1
-	while(gp2040Core1->ready() == false ) {
-		__asm volatile ("nop\n");
-	}
-	gp2040Core0->run();
-
+	// Start Core0 Loop
+	gp2040->run();
 	return 0;
 }
